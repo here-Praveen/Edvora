c4efe96ec9f41727d072205fcbebf26774d0a9e6
Praveen <68317775+here-Praveen@users.noreply.github.com>
2022-07-23 18:51:33 +0530

    Update Edvoratestcaseone.java

--- Edvoratestcaseone.java
+++ Edvoratestcaseone.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,15 +13,39 @@ public class Edvoratestcaseone {
 		
 		driver.get("https://testing-assessment-foh15kew9-edvora.vercel.app/");
 		
+		
+		WebElement register = driver.findElement(By.xpath("//*[@id=\'__next\']/div/main/div/div/p[2]/button"));
+		register.click();
+		
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
+        
+		
 		WebElement uname = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div/div/div[1]/div/input"));
-        uname.sendKeys("Paav");
+        uname.sendKeys("Pwwv");
         
         WebElement pass = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div/div/div[2]/div/input"));
         pass.sendKeys("paas");
         
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
+        
+        Alert al = driver.switchTo().alert();
+        al.accept();
+        
         WebElement Login = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div/div/button"));
         Login.click();
         
+        Alert all = driver.switchTo().alert();
+        all.accept();
         
 
 	}
